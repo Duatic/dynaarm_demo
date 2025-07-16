a1eac41d17db76cbd48a56c336af190e7ad6cb74
akantounias <antonis.kantounias@gmail.com>
2025-07-16 21:08:58 +0200

    Added the freedrive handle

---
+++ dynaarm_end_effectors/launch/freedrive_handle.launch.py
@@ -0,0 +1,93 @@
+# Copyright 2025 Duatic AG
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+# the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions, and
+#    the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions, and
+#    the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or
+#    promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import xacro
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # Load the robot description
+    pkg_share_description = FindPackageShare(package="dynaarm_end_effectors").find(
+        "dynaarm_end_effectors"
+    )
+
+    doc = xacro.parse(
+        open(os.path.join(pkg_share_description, "urdf/freedrive_handle_standalone.urdf.xacro"))
+    )
+    xacro.process_doc(doc, mappings={})
+    robot_description = {"robot_description": doc.toxml()}
+
+    # Publish the joint state values for the non-fixed joints in the URDF file.
+    start_joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+    )
+
+    # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    # Launch RViz
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=[
+            "-d",
+            os.path.join(
+                get_package_share_directory("dynaarm_description"),
+                "config",
+                "config.rviz",
+            ),
+        ],
+        parameters=[robot_description],
+    )
+
+    nodes_to_start = [
+        start_joint_state_publisher_node,
+        robot_state_pub_node,
+        rviz_node,
+    ]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+
+    # Declare the launch arguments
+    declared_arguments = []
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
